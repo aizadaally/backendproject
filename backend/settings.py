d9b2ae694fc4a51072383e6a7b4c3d524fb364f1
aizada <aizada.berdibecova@gmail.com>
2023-12-13 21:14:40 +0600

    database

--- backend/settings.py
+++ backend/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'NAME': 'backend_final',
         'USER': 'saadat',
         'PASSWORD': 'password',
-        'HOST': '127.0.0.1',
+        'HOST': 'localhost',
         'PORT': '3306',
         # 'linux socket': '/var/run/mysqld/mysqld.sock'
     }
